595ecbf5e70319105355029b679ceb1299ef053a
juanborja <juan.borja7@gmail.com>
2021-03-10 00:33:09 -0300

    More input/outputs examples

--- src/main.js
+++ src/main.js
@@ -3,15 +3,18 @@ guiones digitos o espacios formatear a un numero separado en digitos de a tres
 usando guines como separador
 Ejemplos
 
-entrada                               salida
-12  31231----  231    2    ---- 32
+entrada                                       salida
+12     31231----  231    2    ---- 3212       123-123-123-123-212
+12123----  -- - - ---- -21123231              121-232-112-323-1
+12333 --999    1126579                        123-339-991-126-579
+45312---4321 52 2 512 54"                     453-124-321-522-512-54 
 */
 
 export default function main() {
-  const iterative = solution("12     31231----  231    2    ---- 3212");
+  const iterative = solution("12333 --999    1126579");
   console.log(iterative);
 
-  const functional = solution3("12123----  -- - - ---- -21123231");
+  const functional = solution3("45312---4321 52 2 512 54");
   console.log(functional);
 }
 
